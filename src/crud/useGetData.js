c19907a4ee1439e11963265ae493722a674a6d82
osmanramadan <o@o.com>
2024-07-16 18:21:10 +0300

    useget

--- src/crud/useGetData.js
+++ src/crud/useGetData.js
@@ -1,15 +1,21 @@
 import baseUrl from '../api/baseURL';
 
-const useGetData = async (url, parmas) => {
-  const res = await baseUrl.get(url, parmas);
+const useGetData = async (url, params, timeout = 10000) => { // Default timeout of 5000ms
+  const config = {
+    params,
+    timeout,
+  };
+  
+  const res = await baseUrl.get(url, config);
   return res.data;
 };
 
-const useGetDataToken = async (url) => {
+const useGetDataToken = async (url, timeout = 10000) => { // Default timeout of 5000ms
   const config = {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
+    timeout,
   };
 
   const res = await baseUrl.get(url, config);
