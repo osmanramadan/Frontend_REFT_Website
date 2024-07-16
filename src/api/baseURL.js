0c97ccd4d6297b3f5393f9dc5ce173a81efaf27b
osmanramadan <o@o.com>
2024-07-16 04:46:18 +0300

    os

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+// choose between two env
 const baseUrl = axios.create({ baseURL:`${process.env.REACT_APP_NODE_ENV=='dev'?process.env.REACT_APP_DEV_API:process.env.REACT_APP_PROD_API}`});
 
 export default baseUrl;
